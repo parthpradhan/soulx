93db06e61c289af43f264348895b99c5190a9aa9
Parth Pradhan <parthdbz24@gmail.com>
2022-02-23 13:30:49 +0530

    2nd commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import "./contact.scss";
-
+import emailjs from 'emailjs-com';
 export default function Contact() {
   const [message, setMessage] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setMessage(true);
-  };
+    emailjs.sendForm(
+      "service_fxwozhg","template_dvtrmwq",
+      e.target,
+      "user_5wahDSklEwrDVACdD5MF7"
+    ).then(res=>{
+      console.log(res);
+    }).catch(err=>console.log(err));
+  }
   return (
     <div className="contact" id="contact">
       <div className="left">
@@ -16,8 +23,8 @@ export default function Contact() {
       <div className="right">
         <h2>Contact Me</h2>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email" />
-          <textarea placeholder="Message"></textarea>
+          <input name="email" type="text" placeholder="Email" />
+          <textarea name="message" placeholder="Message"></textarea>
           <button type="submit">Send</button>
           {message && <span>Thanks, I'll reply ASAP :)</span>}
         </form>
